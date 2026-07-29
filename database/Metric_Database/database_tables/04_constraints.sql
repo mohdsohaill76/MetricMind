4f85a65400c5cc2dac0b5d56a92ca1cb55d9956f
Harsh-Pc-web <harshsingh2005a@gmail.com>
2026-07-29 21:31:01 +0530

    Updated constraints and added indexes on foreign keys

--- database/Metric_Database/database_tables/04_constraints.sql
+++ database/Metric_Database/database_tables/04_constraints.sql
@@ -27,3 +27,20 @@ add check (discount >= 0);
 
 alter table business.fact_sales
 add check (sales_channel in ('Online','Offline'));
+
+-- Indexes on Foreign Keys
+
+create index idx_fact_sales_customer
+on business.fact_sales(customer_id);
+
+create index idx_fact_sales_product
+on business.fact_sales(product_id);
+
+create index idx_fact_sales_region
+on business.fact_sales(region_id);
+
+create index idx_fact_sales_date
+on business.fact_sales(date_id);
+
+create index idx_fact_sales_salesperson
+on business.fact_sales(salesperson_id);
