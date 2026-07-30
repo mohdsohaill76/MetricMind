7fbc714a7cff91fceefa6f48d02329930cb22e98
Harsh-Pc-web <harshsingh2005a@gmail.com>
2026-07-30 20:29:55 +0530

    Updated database setup documentation

--- database/Metric_Database/database_tables/01_database.sql
+++ database/Metric_Database/database_tables/01_database.sql
@@ -1,1 +1,8 @@
+-- ==========================================
+-- DATABASE SETUP
+-- ==========================================
+-- Run this script only once.
+-- Do not execute if the database already exists.
+-- ==========================================
+
 create database metricmind;
